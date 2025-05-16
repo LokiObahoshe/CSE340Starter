66510bc7df8bb183db6fd3730f6742e486872863
LokiObahoshe <sondereggerbecca@gmail.com>
2025-05-16 16:22:08 -0500

    SQL change

---
+++ database/assignment2.sql
@@ -0,0 +1,40 @@
+-- Insert Tony Stark into Database
+INSERT INTO public.account (
+        account_firstname,
+        account_lastname,
+        account_email,
+        account_password
+    )
+VALUES (
+        'Tony',
+        'Stark',
+        'tony@starkent.com',
+        'Iam1ronM@n'
+    );
+-- Update Tony Stark to be "Admin"
+UPDATE public.account
+SET account_type = 'Admin'
+WHERE account_email = 'tony@starkent.com';
+-- Delete Tony Stark from database
+DELETE FROM public.account
+WHERE account_id = 1;
+-- 4
+UPDATE public.inventory
+SET inv_description = REPLACE(
+        inv_description,
+        'small interiors',
+        'a huge interior'
+    )
+WHERE inv_make = 'GM'
+    AND inv_model = 'Hummer';
+-- 5
+SELECT i.inv_make,
+    i.inv_model,
+    c.classification_name
+FROM public.inventory i
+    INNER JOIN public.classification c ON i.classification_id = c.classification_id
+WHERE c.classification_name = 'Sport';
+-- 6
+UPDATE public.inventory
+SET inv_image = REPLACE(inv_image, '/images/', '/images/vehicles/'),
+    inv_thumbnail = REPLACE(inv_thumbnail, '/images/', '/images/vehicles/');
